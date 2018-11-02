2a0702ac4fa8b041c17c41f01da06efcc47ea5bb
Rodrigo de Lima Cunha <rodrigo908@hotmail.com>
2018-11-02 08:29:36 -0300

    Subindo arquivos

---
+++ config/dbConnection.js
@@ -0,0 +1,23 @@
+/* importar o mongoDB */
+var mongo = require('mongodb');
+
+
+var connMongoDB = function(){
+    console.log('entrou na função de conexão');
+
+    var db = new mongo.Db(
+           'got', 
+           new mongo.Server(
+               'localhost', //string contendo o endereço do servidor
+               27017, //porta de conexão
+               {}
+           ),
+           {} 
+        );
+
+    return db;
+}
+
+module.exports = function(){
+  return connMongoDB;
+}
