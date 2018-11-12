1f5d90ce956457cd9aa01ba8994b8d31203f2389
Rodrigo de Lima Cunha <rodrigo908@hotmail.com>
2018-11-12 06:08:29 -0200

    Atualizando informações

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,21 @@
 module.exports.index = function(application, req, res){
-    res.render('index');      
+    res.render('index', { validacao: {}, dadosForm: {} });      
+}
+
+module.exports.autenticar = function(application, req, res){
+    
+    var dadosForm = req.body;
+
+    req.assert('usuario', 'Usuário não pode ser vázio').notEmpty();
+    req.assert('senha', 'Senha não pode ser vázia').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.render('index', { validacao: erros, dadosForm: dadosForm });
+        return;
+    }
+
+    res.send('Tudo ok');
+
 }
